27deea57c217cf83b1b266f8113eea55e0bfcede
Mariana <94marianag@gmail.com>
2020-06-23 10:47:43 -0500

    [ADD] first steps added

---
+++ src/startup/container.js
@@ -0,0 +1,33 @@
+const { createContainer, asClass, asValue, asFunction } = require("awilix");
+
+//config
+const config = require("../config");
+const app = require(".");
+
+//service
+const { HomeService } = require("../services");
+
+//controller
+const { HomeController } = require("../controllers");
+
+//routes
+const { HomeRoutes } = require("../routes/index.routes");
+const Router = require('../routes');
+
+const container = createContainer();
+
+container
+.register({
+    app: asClass(app).singleton(),
+    router: asFunction(Router).singleton(),
+    config: asValue(config)
+})
+.register({
+    HomeService: asClass(HomeService).singleton()
+}).register({
+    HomeController: asClass(HomeController.bind(HomeController)).singleton()
+}).register({
+    HomeRoutes: asFunction(HomeRoutes).singleton()
+});
+
+module.exports = container;
