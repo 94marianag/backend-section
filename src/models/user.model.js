0b55b3736e6abc218aaebbd7749d4b5770747eb0
Mariana <94marianag@gmail.com>
2020-07-06 12:34:56 -0500

    [ADD] Authentication implemented

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -8,24 +8,24 @@ const UserSchema = new Schema({
     password: {type: String, required: true}
 });
 
-UserSchema.method.toJSON = function(){
+UserSchema.methods.toJSON = function(){
     let user = this.toObject();
     delete user.password;
     return user;
 };
 
-UserSchema.method.comparePasswords = function(password){
+UserSchema.methods.comparePasswords = function(password){
     return compareSync(password, this.password);
 };
 
 UserSchema.pre('save', async function(next){
     const user = this;
 
-    if(!iser.isModified("password")){
+    if(!user.isModified("password")){
         return next();
     }
 
-    const alt = genSaltSync(10);
+    const salt = genSaltSync(10);
     const hashPassword = hashSync(user.password, salt);
     user.password = hashPassword;
     next();
--- src/startup/container.js
+++ src/startup/container.js
@@ -8,7 +8,8 @@ const app = require(".");
 const { HomeService, 
         UserService, 
         IdeaService, 
-        CommentService 
+        CommentService,
+        AuthService 
     } = require("../services");
 
 //controller
@@ -16,7 +17,8 @@ const {
     HomeController, 
     UserController, 
     IdeaController, 
-    CommentController 
+    CommentController,
+    AuthController 
 } = require("../controllers");
 
 //routes
@@ -24,7 +26,8 @@ const {
     HomeRoutes,
     UserRoutes,
     IdeaRoutes,
-    CommentRoutes
+    CommentRoutes,
+    AuthRoutes
 } = require("../routes/index.routes");
 const Routes = require('../routes');
 
@@ -46,17 +49,20 @@ container
     HomeService: asClass(HomeService).singleton(),
     UserService: asClass(UserService).singleton(),
     IdeaService: asClass(IdeaService).singleton(),
-    CommentService: asClass(CommentService).singleton()
+    CommentService: asClass(CommentService).singleton(),
+    AuthService: asClass(AuthService).singleton()
 }).register({
     HomeController: asClass(HomeController.bind(HomeController)).singleton(),
     UserController: asClass(UserController.bind(UserController)).singleton(),
     IdeaController: asClass(IdeaController.bind(IdeaController)).singleton(),
-    CommentController: asClass(CommentController.bind(CommentController)).singleton()
+    CommentController: asClass(CommentController.bind(CommentController)).singleton(),
+    AuthController: asClass(AuthController.bind(AuthController)).singleton(),
 }).register({
     HomeRoutes: asFunction(HomeRoutes).singleton(),
     UserRoutes: asFunction(UserRoutes).singleton(),
     IdeaRoutes: asFunction(IdeaRoutes).singleton(),
-    CommentRoutes: asFunction(CommentRoutes).singleton()
+    CommentRoutes: asFunction(CommentRoutes).singleton(),
+    AuthRoutes: asFunction(AuthRoutes).singleton()
 }).register({
     User: asValue(User),
     Idea: asValue(Idea),
