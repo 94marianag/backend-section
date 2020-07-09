efb910ba6763c8790cdb938f79809e02b9570c06
Mariana <94marianag@gmail.com>
2020-07-09 09:31:09 -0500

    [ADD] Tests added

---
+++ test/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+  user: {
+     _id: "5f0720bbee46762b59d5461a",
+    name: "Mariana",
+    username: "Mariana",
+    password: "pass123"
+  },
+  users: [
+    {
+      _id: "508hsdd833sj32jb34b3b13jb4fr",
+      name: "Anahi",
+      username: "Anahi",
+      password: "pass123"
+    },
+    {
+      _id: "508hsdd833sj32jb345fdgb13jb4",
+      name: "Garcia",
+      username: "Garcia",
+      password: "pass123"
+    }
+  ]
+};
